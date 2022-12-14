8dd9927bb8e7d53c4e31bdb7888115853bb6ced3
Muhammad Gilang Nur Haliz <mgilangnurhaliz@gmail.com>
2022-12-14 23:31:36 +0700

    Membuat method POST atau create todo task

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +12,8 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,19 +24,67 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: titleController,
             decoration: InputDecoration(hintText: 'Title'),
           ),
           SizedBox(height: 20),
           TextField(
+            controller: descriptionController,
             decoration: InputDecoration(hintText: 'Description'),
             keyboardType: TextInputType.multiline,
             minLines: 5,
             maxLines: 8,
           ),
           SizedBox(height: 20),
-          ElevatedButton(onPressed: () {}, child: Text('Submit'))
+          ElevatedButton(onPressed: submitData, child: Text('Submit'))
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    //* Get data dari form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    //* Submit data ke server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri, body: jsonEncode(body), headers: {
+      'Content-Type': 'application/json',
+    });
+
+    //* Show data yang diinput berhasil atau tidak
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      print('Creation Success');
+      showSuccessMessage('Creation Success');
+    } else {
+      showErroMessage('Creation Failed');
+    }
+  }
+
+  //* Message Success
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  //* Message Error
+  void showErroMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
