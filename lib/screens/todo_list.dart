a461889972a27674322be017084937452a03fe03
nafakhairunnisa <nkhairunn2412@gmail.com>
2022-12-18 22:40:20 +0700

    Membuat methodGet untuk menampilkan list Todo Task

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:html';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:todolist_kelompokdua_kelasd/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -10,10 +13,28 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Todo List')),
+        body: ListView.builder(
+          itemsCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index] as Map;
+            return ListTile(
+              leading: CircleAvatar(child: Text('${index + 1}')),
+              title: Text(item['title']),
+              title: Text(item['description']),
+            );
+          },
+        ),
         floatingActionButton: FloatingActionButton.extended(
             onPressed: navigateToAddPage, label: Text('add Todo')));
   }
@@ -21,5 +42,20 @@ class _TodoListPageState extends State<TodoListPage> {
   void navigateToAddPage() {
     final route = MaterialPageRoute(builder: (context) => AddTodoPage());
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'http://api.nstack.in/v1/todos?page=1&limit=10';
+    final url = Uri.parse(url);
+    final response = http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {
+      // Menunjukkan error
+    }
   }
 }
