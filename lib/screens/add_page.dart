132f169502f3ca7cb659af4916c9ec2a4d5e99e7
Naufal Adifa Ramdani <adfar252@gmail.com>
2022-12-26 10:55:00 +0700

    Membuat Fitur Edit Todo Task

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -5,7 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({super.key});
+  final Map? todo;
+  const AddTodoPage({
+    super.key,
+    this.todo,
+  });
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
@@ -14,11 +18,27 @@ class AddTodoPage extends StatefulWidget {
 class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Todo'),
+        title: Text(isEdit ? 'Edit Todo' : 'Add Todo'),
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
@@ -36,10 +56,49 @@ class _AddTodoPageState extends State<AddTodoPage> {
             maxLines: 8,
           ),
           SizedBox(height: 20),
-          ElevatedButton(onPressed: submitData, child: Text('Submit'))
+          ElevatedButton(
+              onPressed: isEdit ? updateData : submitData,
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(isEdit ? 'Update' : 'Submit')))
         ],
       ),
     );
+  }
+
+  Future<void> updateData() async {
+    //get data dari form
+    final todo = widget.todo;
+    if (todo == null) {
+      print('kamu tidak bisa mengupdate data kosong');
+      return;
+    }
+    final id = todo['_id'];
+    final isCompleted = todo['is_Completed'];
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    //* Submit data ke server
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.put(
+      uri,
+      body: jsonEncode(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    //* Show data yang diinput berhasil atau tidak
+    if (response.statusCode == 200) {
+      showSuccessMessage('Updating Success');
+    } else {
+      showErroMessage('Update Failed');
+    }
   }
 
   Future<void> submitData() async {
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -6,7 +6,11 @@ import 'package:todolist_kelompokdua_kelasd/screens/add_page.dart';
 import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
-  const TodoListPage({super.key});
+  final Map? todo;
+  const TodoListPage({
+    super.key,
+    this.todo,
+  });
 
   @override
   State<TodoListPage> createState() => _TodoListPageState();
@@ -37,32 +41,29 @@ class _TodoListPageState extends State<TodoListPage> {
                 final item = items[index] as Map;
                 final id = item['_id'] as String;
                 return ListTile(
-                  leading: CircleAvatar(child: Text('${index + 1}')),
-                  title: Text(item['title']),
-                  subtitle: Text(item['description']),
-                  trailing: PopupMenuButton(
-                    onSelected: (value){
-                      if(value == 'edit'){
+                    leading: CircleAvatar(child: Text('${index + 1}')),
+                    title: Text(item['title']),
+                    subtitle: Text(item['description']),
+                    trailing: PopupMenuButton(onSelected: (value) {
+                      if (value == 'edit') {
                         //open Edit Page
-                      }else if(value == 'delete'){
+                        navigateToEditPage(item);
+                      } else if (value == 'delete') {
                         //Delete and remove the item
                         deleteById(id);
                       }
-                    },
-                    itemBuilder: (context) {
+                    }, itemBuilder: (context) {
                       return [
                         PopupMenuItem(
                           child: Text('Edit'),
                           value: 'edit',
-                  ),
+                        ),
                         PopupMenuItem(
                           child: Text('Delete'),
-                        value: 'delete',
+                          value: 'delete',
                         ),
                       ];
-                    }
-                  )
-                );
+                    }));
               },
             ),
           ),
@@ -71,22 +72,38 @@ class _TodoListPageState extends State<TodoListPage> {
             onPressed: navigateToAddPage, label: Text('add Todo')));
   }
 
-  void navigateToAddPage() {
-    final route = MaterialPageRoute(builder: (context) => AddTodoPage());
+  Future<void> navigateToEditPage(Map item) async {
+    final route =
+        MaterialPageRoute(builder: (context) => AddTodoPage(todo: item));
     Navigator.push(context, route);
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> navigateToAddPage() async {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
   }
 
   Future<void> deleteById(String id) async {
     final url = 'https://api.nstack.in/v1/todos/$id';
     final uri = Uri.parse(url);
     final response = await http.delete(uri);
-    if(response.statusCode ==200){
-
-final filtered = items.where((element) => element['_id'] != id).toList();
-setState((){
-  items = filtered;
-}); 
-    }else{
+    if (response.statusCode == 200) {
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
       showErrorMessage('Deletion Failed');
     }
   }
@@ -106,6 +123,6 @@ setState((){
       isLoading = false;
     });
   }
-  
+
   void showErrorMessage(String s) {}
 }
